f48102da3d563ac367a19409043210b767d1184c
Ryan Wilson <dud313l4ck@hotmail.com>
2014-11-18 14:36:34 -0700

    I changed the JavaMastersUI's Layout from GridLayout to Vertical Layout. I also updated the NavigationMenu so that it is able to dropdown the overviw, practice problems, and helpful links page and navigate to those pages.

--- JavaMasters/src/com/application/javamasters/JavaMastersUI.java
+++ JavaMasters/src/com/application/javamasters/JavaMastersUI.java
@@ -1,23 +1,38 @@
 package com.application.javamasters;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+
+
+
 import javax.servlet.annotation.WebServlet;
 
 import com.application.javamasters.components.NavigationMenu;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.FontAwesome;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.AbsoluteLayout;
 import com.vaadin.ui.Accordion;
-import com.vaadin.ui.Alignment;
-import com.vaadin.ui.GridLayout;
-import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 
 @SuppressWarnings("serial")
 @Theme("valo")
 public class JavaMastersUI extends UI {
+
+	private Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+	private int screenWidth = screenSize.width;
+	
+	private static VerticalLayout mainLayout;
+	private static Panel centerPanel;
+	private static HorizontalLayout horizontalLayout;
+	private static Label title;
 
 	@WebServlet(value = "/*", asyncSupported = true)
 	@VaadinServletConfiguration(productionMode = false, ui = JavaMastersUI.class)
@@ -27,32 +42,202 @@ public class JavaMastersUI extends UI {
 	@Override
 	protected void init(VaadinRequest request) {
 		
-		final GridLayout pageLayout = new GridLayout(4, 3);
-		pageLayout.setMargin(true);
-		pageLayout.setSizeFull();
-		pageLayout.setSpacing(true);
-		setContent(pageLayout);
+		mainLayout = new VerticalLayout();
+		mainLayout.setMargin(true);
+		mainLayout.setSizeFull();
+		mainLayout.setSpacing(true);
+		setContent(mainLayout);
 		
-		Accordion acc = new NavigationMenu();
-		pageLayout.addComponent(acc, 0, 0, 0, 1);
+		mainLayout.addComponent(createHeader());
 		
-		Panel centerPanel = createCenterPanel();
-		pageLayout.addComponent(centerPanel, 1, 0, 2, 2);
+		horizontalLayout = createCenterLayout();
+		mainLayout.addComponent(horizontalLayout);
+		mainLayout.setExpandRatio(horizontalLayout, 1);
 	}
 	
+	/**
+	 * Responsible for creating the NavigationMenu and the panel that contains it.
+	 * 
+	 * @return Panel with the NavigationMenu
+	 */
+	private Panel createNavigation() {
+		
+		Panel menuContainer = new Panel("Navigation");
+		menuContainer.setIcon(FontAwesome.NAVICON);
+		menuContainer.addStyleName("light"); // No border
+		menuContainer.setWidth("300px"); // Undefined width
+		menuContainer.setHeight("100%");
+		
+		final VerticalLayout verticalNavigationLayout = new VerticalLayout();
+		menuContainer.setHeight(null);	
+		Accordion accordion = new NavigationMenu();
+		menuContainer.setContent(verticalNavigationLayout);
+		verticalNavigationLayout.addComponent(accordion);
+
+		return menuContainer;
+	}
+	
+	/**
+	 * 
+	 * 
+	 * @return 
+	 */
+	private HorizontalLayout createCenterLayout()
+	{
+		HorizontalLayout hLayout = new HorizontalLayout();
+		hLayout.setSpacing(true);
+		hLayout.setHeight("100%");
+		
+		Panel navContainer = createNavigation();
+		hLayout.addComponent(navContainer);
+		
+		centerPanel = createCenterPanel();
+		hLayout.addComponent(centerPanel);
+		
+		return hLayout;
+	}
+	
+	/**
+	 * Creates and returns a Panel that is located at the center of the UI
+	 * and contains the major content of the program.
+	 * 
+	 * @return Panel with a Label
+	 */
 	private Panel createCenterPanel() 
 	{
 		Panel centerPanel = new Panel();
 		centerPanel.setSizeFull();
-		GridLayout centerPanelLayout = new GridLayout(1, 3);
-		centerPanelLayout.setSizeFull();
+		AbsoluteLayout centerPanelLayout = new AbsoluteLayout();
+		centerPanelLayout.setWidth(new String(2.25 * screenWidth / 3 + "px"));
+		centerPanelLayout.setHeight("100%");
 		centerPanel.setContent(centerPanelLayout);
 		
 		Label headerLabel = new Label("Welcome to Java Masters Learning Software");
 		centerPanelLayout.addComponent(headerLabel);
-		centerPanelLayout.setComponentAlignment(headerLabel, Alignment.MIDDLE_CENTER);
 		
 		return centerPanel;
 	}
-
+	
+	/**
+	 * A method that creates the Header/Title label at the top of the page.
+	 * 
+	 * @return Horizontal Layout with a Label
+	 */
+	private HorizontalLayout createHeader() {
+		
+		HorizontalLayout header = new HorizontalLayout();
+		header.setWidth("100%");
+		title = new Label("Welcome to JavaMasters"); 
+		header.addComponent(title);
+		
+		title.addStyleName("huge");
+		title.addStyleName("color1");
+		
+		return header;
+	}
+	
+	/**
+	 * A method that changes the central panel to display information
+	 * about either Overview, Practice Problems, or Helpful Links.
+	 * 
+	 * @param main topic
+	 * @param sub topic
+	 * @param Overview, Practice Problems, or Helpful Links
+	 */
+	public static void changePage(String mainTopic, String subTopic, String page)
+	{
+		horizontalLayout.removeComponent(centerPanel);
+		switch (mainTopic)
+		{
+			case "Variables":
+				switch (subTopic)
+				{
+					case "Variable Declaring / Instantiation":
+						switch (page)
+						{
+							case "Overview":
+								title.setCaption("Variables  |  Variable Declaring / Instantiation  |  Helpful Links");	//This is just a test...
+//								horizontalLayout.addComponent(centerPanel = new com.application.javamasters.views.Overview(
+//										"https://www.google.com/?gws_rd=ssl",
+//										"https://www.google.com/?gws_rd=ssl",
+//										"https://www.google.com/?gws_rd=ssl")););
+								break;
+							case "Practice Problems":
+								break;
+							case "Helpful Links":
+								title.setCaption("Variables  |  Variable Declaring / Instantiation  |  Helpful Links");	//This is just a test...
+								horizontalLayout.addComponent(centerPanel = new com.application.javamasters.views.HelpfulLinks(
+										"https://www.google.com/?gws_rd=ssl",
+										"https://www.google.com/?gws_rd=ssl",
+										"https://www.google.com/?gws_rd=ssl"));
+								break;
+						}
+						break;
+					case "Int":
+						break;
+					case "Double / Float":
+						break;
+					case "String / Char":
+						break;
+					case "Boolean":
+						break;
+					case "Scanner":
+						break;
+				}
+				break;
+				
+			case "Control Statements 1":
+				switch (subTopic)
+				{
+					case "Selection Statements":
+						break;
+					case "Logic Operators":
+						break;
+					case "Increment / Decrement":
+						break;
+				}
+				break;
+				
+			case "Control Statements 2":
+				switch (subTopic)
+				{
+					case "For Loop":
+						break;
+					case "While / Do While":
+						break;
+					case "For Each Loop":
+						break;
+				}
+				break;
+				
+			case "Methods and Classes":
+				switch (subTopic)
+				{
+					case "Classes - Declare/Create/Access":
+						break;
+					case "Methods":
+						break;
+					case "Getters / Setters":
+						break;
+					case "Constructors":
+						break;
+				}
+				break;
+				
+			case "Arrays":
+				switch (subTopic)
+				{
+					case "Passing Arrays as Arguments":
+						break;
+					case "Methods":
+						break;
+					case "Class Array":
+						break;
+					case "Array List <E>":
+						break;
+				}
+				break;
+			
+		}
+	}
 }
--- JavaMasters/src/com/application/javamasters/components/NavigationMenu.java
+++ JavaMasters/src/com/application/javamasters/components/NavigationMenu.java
@@ -1,18 +1,23 @@
 package com.application.javamasters.components;
 
+
+import com.application.javamasters.JavaMastersUI;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Accordion;
-import com.vaadin.ui.Button;
+import com.vaadin.ui.MenuBar;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.Panel;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.MenuBar.Command;
+import com.vaadin.ui.MenuBar.MenuItem;
 
 
 @SuppressWarnings("serial")
-@Theme("vaadindemo")
+@Theme("valo")
 public class NavigationMenu extends Accordion
-{
+{	
 	@VaadinServletConfiguration(productionMode = false, ui = com.application.javamasters.JavaMastersUI.class)
 	public static class Servlet extends VaadinServlet {
 	}
@@ -28,17 +33,17 @@ public class NavigationMenu extends Accordion
 		mainTopic_Variables.setContent(variablesLayout);
 		addTab(mainTopic_Variables, "Variables");
 		
-		Button subTopic_VarDec = createAccordionButton("Variable Declaring / Instantiation");
+		MenuBar subTopic_VarDec = getMenuBar("Variables", "Variable Declaring / Instantiation");
 		variablesLayout.addComponent(subTopic_VarDec);
-		Button subTopic_Int = createAccordionButton("Int");
+		MenuBar subTopic_Int = getMenuBar("Variables", "Int");
 		variablesLayout.addComponent(subTopic_Int);
-		Button subTopic_DoubleFloat = createAccordionButton("Double / Float");
+		MenuBar subTopic_DoubleFloat = getMenuBar("Variables", "Double / Float");
 		variablesLayout.addComponent(subTopic_DoubleFloat);
-		Button subTopic_StringChar = createAccordionButton("String / Char");
+		MenuBar subTopic_StringChar = getMenuBar("Variables", "String / Char");
 		variablesLayout.addComponent(subTopic_StringChar);
-		Button subTopic_Boolean = createAccordionButton("Boolean");
+		MenuBar subTopic_Boolean = getMenuBar("Variables", "Boolean");
 		variablesLayout.addComponent(subTopic_Boolean);
-		Button subTopic_Scanner = createAccordionButton("Scanner");
+		MenuBar subTopic_Scanner = getMenuBar("Variables", "Scanner");
 		variablesLayout.addComponent(subTopic_Scanner);
 		
 		
@@ -48,11 +53,11 @@ public class NavigationMenu extends Accordion
 		mainTopic_CS1.setContent(controlStatements1Layout);
 		addTab(mainTopic_CS1, "Control Statements 1");
 		
-		Button subTopic_SelectionStatements = createAccordionButton("Selection Statements");
+		MenuBar subTopic_SelectionStatements = getMenuBar("Control Statements 1", "Selection Statements");
 		controlStatements1Layout.addComponent(subTopic_SelectionStatements);
-		Button subTopic_LogicOperators = createAccordionButton("Logic Operators");
+		MenuBar subTopic_LogicOperators = getMenuBar("Control Statements 1", "Logic Operators");
 		controlStatements1Layout.addComponent(subTopic_LogicOperators);
-		Button subTopic_IncDec = createAccordionButton("Increment / Decrement");
+		MenuBar subTopic_IncDec = getMenuBar("Control Statements 1", "Increment / Decrement");
 		controlStatements1Layout.addComponent(subTopic_IncDec);
 		
 		
@@ -62,11 +67,11 @@ public class NavigationMenu extends Accordion
 		mainTopic_CS2.setContent(controlStatements2Layout);
 		addTab(mainTopic_CS2, "Control Statements 2");
 		
-		Button subTopic_ForLoop = createAccordionButton("For Loop");
+		MenuBar subTopic_ForLoop = getMenuBar("Control Statements 2", "For Loop");
 		controlStatements2Layout.addComponent(subTopic_ForLoop);
-		Button subTopic_WhileDoWhile = createAccordionButton("While / Do While");
+		MenuBar subTopic_WhileDoWhile = getMenuBar("Control Statements 2", "While / Do While");
 		controlStatements2Layout.addComponent(subTopic_WhileDoWhile);
-		Button subTopic_ForEachLoop = createAccordionButton("For Each Loop");
+		MenuBar subTopic_ForEachLoop = getMenuBar("Control Statements 2", "For Each Loop");
 		controlStatements2Layout.addComponent(subTopic_ForEachLoop);
 		
 		
@@ -76,13 +81,13 @@ public class NavigationMenu extends Accordion
 		mainTopic_MethodsClasses.setContent(methodsClassesLayout);
 		addTab(mainTopic_MethodsClasses, "Methods and Classes");
 		
-		Button subTopic_Classes = createAccordionButton("Classes - Declare/Create/Access");
+		MenuBar subTopic_Classes = getMenuBar("Methods and Classes", "Classes - Declare/Create/Access");
 		methodsClassesLayout.addComponent(subTopic_Classes);
-		Button subTopic_Methods = createAccordionButton("Methods");
+		MenuBar subTopic_Methods = getMenuBar("Methods and Classes", "Methods");
 		methodsClassesLayout.addComponent(subTopic_Methods);
-		Button subTopic_GettersSetters = createAccordionButton("Getters / Setters");
+		MenuBar subTopic_GettersSetters = getMenuBar("Methods and Classes", "Getters / Setters");
 		methodsClassesLayout.addComponent(subTopic_GettersSetters);
-		Button subTopic_Constructors = createAccordionButton("Constructors");
+		MenuBar subTopic_Constructors = getMenuBar("Methods and Classes", "Constructors");
 		methodsClassesLayout.addComponent(subTopic_Constructors);
 		
 		
@@ -92,21 +97,41 @@ public class NavigationMenu extends Accordion
 		mainTopic_Arrays.setContent(arraysLayout);
 		addTab(mainTopic_Arrays, "Arrays");
 		
-		Button subTopic_ArrayArguments = createAccordionButton("Passing Arrays as Arguments");
+		MenuBar subTopic_ArrayArguments = getMenuBar("Arrays", "Passing Arrays as Arguments");
 		arraysLayout.addComponent(subTopic_ArrayArguments);
-		Button subTopic_Arrays = createAccordionButton("Methods");
+		MenuBar subTopic_Arrays = getMenuBar("Arrays", "Methods");
 		arraysLayout.addComponent(subTopic_Arrays);
-		Button subTopic_ClassArray = createAccordionButton("Class Array");
+		MenuBar subTopic_ClassArray = getMenuBar("Arrays", "Class Array");
 		arraysLayout.addComponent(subTopic_ClassArray);
-		Button subTopic_ArrayList = createAccordionButton("ArrayList<E>");
+		MenuBar subTopic_ArrayList = getMenuBar("Arrays", "Array List <E>");
 		arraysLayout.addComponent(subTopic_ArrayList);
 	}
 	
-	private Button createAccordionButton(String text) 
-	{
-		Button button = new Button(text);
-		button.setWidth("100%");
-		
-		return button;
-	}
+	/**
+	 * Returns a MenuBar that has the functionality to change pages
+	 * depending on what topic, subtopic, and page the user selects.
+	 * 
+	 * @param mainTopic
+	 * @param subTopic
+	 * @return MenuBar
+	 */
+	static MenuBar getMenuBar(final String mainTopic, final String subTopic) {
+        Command click = new Command() {
+            @Override
+            public void menuSelected(MenuItem selectedItem) {
+                Notification.show("Clicked " + mainTopic + "   |   " + subTopic + "   |   " + selectedItem.getText());
+                
+                JavaMastersUI.changePage(mainTopic, subTopic, selectedItem.getText());
+            }
+        };
+        
+        MenuBar menubar = new MenuBar();
+        menubar.setWidth("100%");
+        final MenuBar.MenuItem file = menubar.addItem(subTopic, null);
+        file.addItem("Overview", click);
+        file.addItem("Practice Problems", click);
+        file.addItem("Helpful Links", click);
+
+        return menubar;
+    }
 }
